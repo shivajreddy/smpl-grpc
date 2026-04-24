3e38930d92a2edd02b41b23c1e13354621f7ee6f
smpl <sreddy@hey.com>
2026-04-24 11:52:12 -0700

    .

---
+++ hello/Program.cs
@@ -0,0 +1,11 @@
+using System;
+using Google.Protobuf;
+
+//Create and populate
+var p1 = new Person {Name = "smpl", Age = 30};
+Console.WriteLine($"p1:: Name: {p1.Name}, Age: {p1.Age}");
+// serialize to bytes
+byte[] bytes = p1.ToByteArray();
+// Deserialize from bytes
+var p2 = Person.Parser.ParseFrom(bytes);
+Console.WriteLine($"p2:: Name: {p2.Name}, Age: {p2.Age}");
